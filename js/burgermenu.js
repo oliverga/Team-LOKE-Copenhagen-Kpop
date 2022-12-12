56829b902f9a87efa1951819b14676ce35a03f1a
oliverga <hi@oliverga.com>
2022-12-12 09:43:06 +0100

    header burger menu v1

---
+++ js/burgermenu.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector(".toggle-btn");
+const menuMobile = document.querySelector(".header_nav_menu_container_mobile");
+
+function toggleMenu() {
+  menuMobile.classList.toggle("shown");
+
+  const menuShown = menuMobile.classList.contains("shown");
+
+  if (menuShown) {
+    console.log(menuShown); 
+    btn.textContent = "Luk";
+  } else {
+    console.log(menuShown); 
+    btn.textContent = "Menu";
+  }
+}
+
+btn.addEventListener("click", toggleMenu);
